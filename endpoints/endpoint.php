689b271b83f7fa2af2e23375aace8096e927aa5f
vaibhav <vaibhav.111padghan@gmail.com>
2022-04-02 16:02:44 +0530

    first commit

---
+++ endpoints/endpoint.php
@@ -0,0 +1,27 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/partials/db/db.php';
+$response = null;
+try {
+
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+    } else {
+        throw new Exception("Username or Passoword is missing!");
+    }
+
+    $sql = "SELECT * FROM `cf`.`users` WHERE `username`='$username' AND `password`='$password'";
+    $result = $conn->query($sql);
+    if (!$result->num_rows > 0) {
+        throw new Exception("Invali credentials!");
+    }
+
+    $response['msg'] = "Login successfull!";
+    $response['result'] = true;
+} catch (Exception $e) {
+    $response['status'] = false;
+    $response['msg'] = $e->getMessage();
+}
+
+$response = json_encode($response);
+echo $response;
