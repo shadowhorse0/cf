a685fe3761c5c5dcfd3c56f5257bed8a8d9449f4
vaibhav <vaibhav.111padghan@gmail.com>
2022-04-03 13:27:53 +0530

    true false bug

--- scripts/api.js
+++ scripts/api.js
@@ -4,7 +4,8 @@ async function endpoint(data, request_type) {
     // encrypting request type
 
     var settings = {
-        "url": "http://128.199.25.59/endpoints/endpoint.php",
+        // "url": "http://128.199.25.59/endpoints/endpoint.php",
+        "url": "http://localhost/endpoints/endpoint.php",
         "method": "POST",
         "dataType": "json",
         "async": true,
