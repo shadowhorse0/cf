a2dc84cc5a6886584b4f1e5a1e80d408f89f41e1
vaibhav <vaibhav.111padghan@gmail.com>
2022-05-02 17:46:21 +0530

    scripts api version change

--- admin/questions/index.php
+++ admin/questions/index.php
@@ -103,7 +103,7 @@ if (isset($_GET['id'])) {
     <!-- jquery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="/scripts/api.js?17"></script>
+    <script src="/scripts/api.js?18"></script>
 
 
     <script>
